2793985ac3295395bce0e195382edaf31f73e995
haileybaileyo <dreamchick07>
2024-10-05 13:09:10 +0900

    시작

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import './Header.css';
+
+const Header = ()=>{
+    return <div className='Header'>
+        <div className='Portfolio'>
+            <h3>PORTFOLIO</h3>
+        </div>
+        <div className='Right'>   
+            <div className='About'>
+                <h3>ABOUT</h3>
+            </div>
+            <div className='Goal'>
+                <h3>GOAL</h3>
+            </div>
+        </div>
+    </div>
+} 
+
+export default Header;
